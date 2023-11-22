a33ab3021ff77b747037880124f352dd0de0350b
Oliver <oliverfruergaard@gmail.com>
2023-11-22 21:45:20 +0100

    make makeMove function and cretePlayer function

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-(function() {
-    const gameboard = {
-    board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+const gameboard = (function() {
+    let makeMove = (player, position) => {
+        gameboard.board.splice((position - 1), 1, player.symbol);
+    };
+    const board = {
+    board: ['','','','','','','','',''],
+    makeMove,
 };
-return gameboard;
+return board;
 })();
 
-const createPlayer = () => {
-    handle = prompt('Enter handle');
-    symbol = prompt('Enter symbol');
+const createPlayer = (handle, symbol) => {
     const player = {
         handle,
         symbol,
     };
-
     player.alertSelection = () => {
         console.log(`${handle}'s symbol is ${symbol}.`);
     };
-
     return player;
 };
 
 const game = (() => {
-    const playerOne = createPlayer();
+    const playerOne = createPlayer('Oliver', 'X');
     playerOne.alertSelection();
-    const playerTwo = createPlayer();
+    const playerTwo = createPlayer('Ilse', 'O');
     playerTwo.alertSelection();
+    gameboard.makeMove(playerOne, 3);
 })();
